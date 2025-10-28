b9160d523035540556f1fc6eecbeddedd90f2491
Navrose Johal <johnavro@sheridancollege.ca>
2025-10-28 13:14:17 -0400

    request file and response file for simulating client server interaction created

---
+++ week 8/w8-inclass/inclass1/http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const options = { host: '127.0.0.1',
+port: 8887,
+path: '/',
+method: 'GET' };
+const req = http.request(options, (res) => {
+console.log(res.statusCode);
+res.on('data', msg => {
+process.stdout.write(msg)
+})
+})
+req.on('error', error => { console.log(error); })
+req.end()
---
+++ week 8/w8-inclass/inclass1/iserver.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 8887;
+http.createServer((req, res) => {
+console.log('Header: ', req.headers);
+console.log('Method: ',req.method);
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.write('This is from Local Server\n');
+res.end();
+}).listen(port,() => {
+console.log(`Server running at
+http://${hostname}:${port}/`);
+}); 27
+Status
+code
+Node.js / In-Class
